e06eaa3d80562475b14f3230a194d7800f7cac8a
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2023-08-07 17:07:30 +0200

    Add files via upload

---
+++ 12_SpreadsheetCellExplitlyDefaulted/main.cpp
@@ -0,0 +1,18 @@
+import spreadsheet_cell;
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+	SpreadsheetCell myCell;
+	myCell.setValue(6);
+	cout << "cell 1: " << myCell.getValue() << endl;
+
+	auto smartCellp{ make_unique<SpreadsheetCell>() };
+	
+	SpreadsheetCell* myCellp{ new SpreadsheetCell };
+	delete myCellp;    myCellp = nullptr;
+	return 0;
+}
