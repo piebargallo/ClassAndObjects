a70f8007ca9950af7eafb7559cbd8a72f98698ca
MIGUEL PIE BARGALLO <90567646+piebargallo@users.noreply.github.com>
2024-07-01 16:04:46 +0200

    Add files via upload

---
+++ PersonReference/PersonReference.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using std::string;
+using std::ostream;
+using std::cout;
+using std::endl;
+
+class Person
+{
+public:
+	Person();
+	Person(string firstName, string lastName, int age);
+	Person(const Person& rhs);
+	virtual ~Person();
+
+	const string& getFirstName() const { return m_firstName; }
+	const string& getLastName() const { return m_lastName; }
+	int getAge() const { return m_age; }
+
+private:
+	string m_firstName, m_lastName;
+	int m_age{ 0 };
+};
+
+Person::Person() {}
+
+Person::Person(string firstName, string lastName, int age)
+	: m_firstName{ move(firstName) }, m_lastName{ move(lastName) }, m_age{ age } {}
+
+Person::~Person() {}
+
+Person::Person(const Person& rhs)
+	: m_firstName{ rhs.m_firstName }, m_lastName{ rhs.m_lastName }, m_age{ rhs.m_age } {}
+
+ostream& operator<<(ostream& ostr, const Person& p)
+{
+	ostr << p.getFirstName() << " " << p.getLastName() <<
+		" " << p.getAge() << endl;
+
+	return ostr;
+}
+
+void processPerson(const Person& p)
+{
+	cout << p <<endl;
+}
+
+int main()
+{
+	Person me{ "Marc", "Gregoire", 42 };
+	processPerson(me);
+}
